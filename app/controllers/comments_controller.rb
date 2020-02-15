b8dd2af068cc61fef0298d12f086fc61957652dd
pandawani <60386602+pandawani@users.noreply.github.com>
2020-02-15 23:53:12 +0900

    コメント投稿フォームの書き換え
    コメント投稿フォームをinputタグからヘルパーメソッドform_forを使って書き換えました。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,20 @@
 class CommentsController < ApplicationController
-def index
-end
+  
+  def index
+    @group = Group.find(params[:group_id])
+    @comment = Comment.new
+    @comments = @group.comments.includes(:user)
+  end
 
-def create
-end
+  # def create
+  #   @comment = Coment.create(comment_params)
+  #   redirect_to "/groups/#{comment.group.id}"
+  # end
+
+  # private
+  # def comment_params
+  #   params.require(:comment).permit(:text).merge(user_id: current_user.id, group_id: params[:group_id])
+  # end
 
 
 end
