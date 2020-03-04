258e4b30a03a890090f8c547d6e0d9220e8dab55
pandawani <60386602+pandawani@users.noreply.github.com>
2020-03-04 22:37:27 +0900

    非同期通信を実装しました

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,19 +9,20 @@ class CommentsController < ApplicationController
   def create
     @group = Group.find(params[:group_id])
     @comment = @group.comments.new(comment_params)
-    if @comment.save
-      flash[:notice]='メッセージが保存されました。'
-      redirect_to group_comments_path(@group)
-    else
-      flash[:alert]='メッセージを入力してください。'
-      render :index
+
+    respond_to do |format|
+      if @comment.save
+        format.json
+      else
+        format.json
+      end
     end
+
   end
 
   def show
     @comment = Comment.new
     @comments = @group.comments.includes(:user)
-    # render :index
   end
 
   private
