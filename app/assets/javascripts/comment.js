7d9cb24be99bc003a3c3f9d1eef3429f0f723935
pandawani <60386602+pandawani@users.noreply.github.com>
2020-03-12 20:43:01 +0900

    自動更新機能の実装
    自動更新機能を実装しました！

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -2,9 +2,9 @@ $(function(){
 
   function buildHTML(comment){
     if(comment.image){
-      let html =
-      `<div class="post-info">
-        <div class="post-info__index">
+      let html = 
+      `<div class="post-info" data-comment-id=${comment.id}>
+        <div class="post-info__index ">
           <p class="post-info__index__name">${comment.user_name}</p>
           <p class="post-info__index__time">${comment.created_at}</p>
         </div>     
@@ -18,7 +18,7 @@ $(function(){
       return html;  
     }else{
       let html =
-      `<div class="post-info">
+      `<div class="post-info" data-comment-id=${comment.id}>
         <div class="post-info__index">
           <p class="post-info__index__name">${comment.user_name}</p>
           <p class="post-info__index__time">${comment.created_at}</p>
@@ -53,4 +53,30 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     })
   })
+
+  let reloadComments = function(){
+    let last_comment_id = $('.post-info:last').data("comment-id");
+    $.ajax({
+      url: "api/comments",
+      type: 'get',
+      dataType:'json',
+      data: {id:last_comment_id}
+    })
+    .done(function(comments){
+      if(comments.length !== 0){
+        let insertHTML = '';
+        $.each(comments, function(i,comment){
+          insertHTML += buildHTML(comment)
+        });
+        $('.chat_main__message-list').append(insertHTML);
+        $('.chat_main__message-list').animate({scrollTop: $('.chat_main__message-list')[0].scrollHeight});
+      }
+    })
+    .fail(function(){
+      alert('error');
+    })
+  }
+  if (document.location.href.match(/\/groups\/\d+\/comments/)) {
+    setInterval(reloadComments, 7000);
+  }
 });
